ad40be307d8ba67deda18046b85a3de036beb117
Madhuj Agrawal <madhuj.2004@yahoo.com>
2025-05-15 15:45:08 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="logo.png" />
+        <link rel="icon" href="logo.jpg" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
